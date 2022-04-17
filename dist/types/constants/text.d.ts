7cd1a330e9c0e39d8e528fedb3379fce1821261a
John Datserakis <johndatserakis@gmail.com>
2022-04-17 13:51:14 -0400

    Update typing

---
+++ dist/types/constants/text.d.ts
@@ -0,0 +1,3 @@
+export declare const DEFAULT_CHOOSE_FILE_TEXT = "Choose file...";
+export declare const DEFAULT_BROWSE_TEXT = "Browse";
+export declare const DEFAULT_FILES_SELECTED_TEXT = "files selected";

b11fa412935075e2fc285e6fe79a550c78e2fcbe
John Datserakis <johndatserakis@gmail.com>
2022-04-23 20:22:52 -0400

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   moduleFileExtensions: ['js', 'ts'],
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(scss|less|svg|png)$': './jest/style-mock.ts',
     '^.+\\.[t|j]sx?$': 'babel-jest',

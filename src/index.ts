4880ce76f45498a9c330518aa04f320e10c0b178
John Datserakis <johndatserakis@gmail.com>
2024-09-02 16:19:18 -0400

    Update build settings (#83)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import './index.scss'; // Importing the CSS allows it to be exported in the build
 
+// Constants
 export * from './constants/events';
+export * from './constants/file';
 export * from './constants/images';
-export * from './file-upload-with-preview';
+export * from './constants/style';
+export * from './constants/text';
+
+// Types
 export * from './types/events';
+export * from './types/options';
+
+// Lib
+export * from './file-upload-with-preview';
